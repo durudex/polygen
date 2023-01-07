374062d060306c7025c84d8973cad993465b3b8f
V1def <vaniya8871@gmail.com>
2023-01-07 14:36:24 +0100

    feat: add config

---
+++ config/config.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2022-2023 Durudex
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type (
+	Config struct {
+		Collections []string `yaml:"collection"`
+		Language    Language `yaml:"language"`
+	}
+
+	Language struct {
+		Go *Go `yaml:"go"`
+	}
+
+	Go struct {
+		Package   string `yaml:"package"`
+		Directory string `yaml:"directory"`
+	}
+)
+
+func New(filename string) (*Config, error) {
+	var cfg Config
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	if err := yaml.NewDecoder(f).Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
