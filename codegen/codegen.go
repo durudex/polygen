109c592afb9a1f67e3bf0de4eb80900584701ee3
V1def <vaniya8871@gmail.com>
2023-01-07 21:39:36 +0100

    feat(codegen): add basic

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -9,8 +9,10 @@ package codegen
 
 import (
 	"context"
+	"os"
 
 	"github.com/durudex/polygen/config"
+	"github.com/durudex/polygen/language/golang"
 
 	"github.com/durudex/go-polybase"
 )
@@ -36,15 +38,42 @@ func New(cfg *config.Config) Codegen {
 }
 
 func (c *codegen) Generate() error {
+	var gn golang.Golang
+
+	if c.cfg.Language.Go != nil {
+		gn = golang.New(c.cfg.Language.Go)
+
+		if err := c.checkDir(c.cfg.Language.Go.Package); err != nil {
+			return err
+		}
+	}
+
 	for _, id := range c.cfg.Collections {
 		ast, err := c.astCollection(context.Background(), id)
 		if err != nil {
 			return err
 		}
 
-		c.parseCollection(ast)
+		parsed := c.parseCollection(ast)
 
-		// TODO
+		if c.cfg.Language.Go != nil {
+			if err := gn.Generate(parsed); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *codegen) checkDir(path string) error {
+	if err := os.MkdirAll(path, 0755); err != nil {
+		if os.IsExist(err) {
+			// TODO: add delete old files by option
+			return nil
+		}
+
+		return err
 	}
 
 	return nil
--- language/golang/golang.go
+++ language/golang/golang.go
@@ -7,4 +7,36 @@
 
 package golang
 
-func Generate() {}
+import (
+	"os"
+
+	"github.com/durudex/polygen"
+	"github.com/durudex/polygen/config"
+	"github.com/durudex/polygen/language/golang/template"
+
+	"github.com/iancoleman/strcase"
+)
+
+type Golang interface {
+	Generate(*polygen.ParsedCollection) error
+}
+
+type golang struct{ cfg *config.Go }
+
+func New(cfg *config.Go) Golang {
+	return &golang{cfg: cfg}
+}
+
+func (g *golang) Generate(coll *polygen.ParsedCollection) error {
+	path := g.cfg.Directory + "/" + strcase.ToSnake(coll.Name) + "_gen.go"
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	template.WriteModel(f, coll.Models)
+
+	return nil
+}
