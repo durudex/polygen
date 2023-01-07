d14b72b7361bf446533915959f7efcff91f4fa21
V1def <vaniya8871@gmail.com>
2023-01-07 19:37:04 +0100

    Added basic ast parser and codegen mock

---
+++ codegen/codegen.go
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2022-2023 Durudex
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package codegen
+
+import (
+	"context"
+
+	"github.com/durudex/polybase-gen/config"
+
+	"github.com/durudex/go-polybase"
+)
+
+type Codegen interface {
+	Generate() error
+}
+
+type codegen struct {
+	cfg  *config.Config
+	coll polybase.Collection
+}
+
+func New(cfg *config.Config) Codegen {
+	client := polybase.New(polybase.Config{
+		URL: polybase.TestnetURL,
+	})
+
+	return &codegen{
+		cfg:  cfg,
+		coll: client.Collection(GenesisCollectionID),
+	}
+}
+
+func (c *codegen) Generate() error {
+	for _, id := range c.cfg.Collections {
+		ast, err := c.astCollection(context.Background(), id)
+		if err != nil {
+			return err
+		}
+
+		c.parseCollection(ast)
+
+		// TODO
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/durudex/polybase-gen
 go 1.19
 
 require (
+	github.com/durudex/go-polybase v0.0.0-20230101154909-ddda04565735
+	github.com/durudex/go-polylang v0.0.0-20230107180402-fae5fcac0a78
 	github.com/rs/zerolog v1.28.0
 	github.com/spf13/cobra v1.6.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/alecthomas/participle/v2 v2.0.0-beta.5 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
