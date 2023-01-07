5a9c4b5a565ca4cb83289fe6d4ca6475c838a42e
V1def <vaniya8871@gmail.com>
2023-01-07 17:04:58 +0100

    feat(cli): add app

--- cmd/polybase-gen/main.go
+++ cmd/polybase-gen/main.go
@@ -1,7 +1,43 @@
+/*
+ * Copyright © 2022-2023 Durudex
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var (
+	configPath string
+
+	root = &cobra.Command{
+		Use:   "polybase-gen",
+		Short: "CLI for generating Polybase Collections API code.",
+		Long:  "polybase-gen is a CLI that generates the API code of the collections you specify.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if configPath == "" {
+				log.Fatal().Msg("Config file not specified!")
+			}
+		},
+	}
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	root.Flags().StringVarP(&configPath, "config", "c", "", "path to config file")
+}
 
 func main() {
-	fmt.Println("Hello Durudex")
+	if err := root.Execute(); err != nil {
+		log.Fatal().Err(err).Msg("error executing cli")
+	}
 }
